f7292bb7de0d80ad5bd5d8603dfcd3bebd2d2d77
naabdykalykov <znslt24@gmail.com>
2026-02-24 23:44:43 +0300

    add prettier and fix format :)

---
+++ prettier.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import("prettier").Config} */
+const config = {
+  semi: true,
+  singleQuote: false,
+  trailingComma: "all",
+  tabWidth: 2,
+  printWidth: 80,
+  jsxSingleQuote: false,
+  endOfLine: "auto",
+};
+
+export default config;
